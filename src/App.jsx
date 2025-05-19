c828f15bde94e7f8acf5d93ee98192116e6a69f2
devsagarkumarjha <devsagarkumarjha@gmail.com>
2025-05-19 16:21:07 +0530

    wishlist form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { HeartPlus } from "lucide-react";
+import { WishlistForm, WishlistItem } from "./components";
+import { useState } from "react";
+
 function App() {
-  return <>
-  <h1>Wishlist</h1>
-  </>;
+
+  const array = [];
+  const [items, setItems] = useState(array);
+
+  const addItem = (item) => setItems([...items, item]);
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-gray-100 p-6">
+      <h1 className="text-3xl font-bold flex justify-center items-center mb-6 gap-2.5">
+        <HeartPlus size={50} className="bg-red-600 p-2 rounded-full" /> Wishlist
+        Wizard
+      </h1>
+
+      <WishlistForm onAddItem={addItem} />
+
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,2 @@
+export { default as WishlistForm } from "./WishlistForm.jsx";
+
