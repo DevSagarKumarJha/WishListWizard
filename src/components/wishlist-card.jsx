a8bd84e44e9760f749db645aaa1f095911b9d9f5
devsagarkumarjha <devsagarkumarjha@gmail.com>
2026-02-10 18:34:25 +0530

    refactor(wishlist-card): tweak card layout and colors - reorder description and category elements for better flow - switch gray to zinc color classes for a nicer look - adjust icon sizes and button spacing - add responsive font size for card title

--- src/components/wishlist-card.jsx
+++ src/components/wishlist-card.jsx
@@ -2,22 +2,31 @@ import { Check, Pencil, Trash2 } from "lucide-react";
 
 function WishlistCard({ item, onToggle, onEdit, onDelete }) {
   return (
-    <div className="bg-gray-900 p-4 rounded-lg flex flex-col justify-between">
-      <div>
-        <h3 className={item.completed ? "line-through text-gray-400" : "font-bold"}>
+    <div className="bg-zinc-900 p-4 rounded-lg flex flex-col justify-between">
+      <div className="flex justify-between items-center text-lg sm:text-xl">
+        <h3
+          className={
+            item.completed ? "line-through text-zinc-200" : "font-bold"
+          }
+        >
           {item.name}
         </h3>
-        <p className="text-sm text-gray-400">{item.description}</p>
-        <span className="text-xs bg-gray-800 px-2 py-1 rounded mt-2 inline-block">
-          {item.category}
-        </span>
+        <div className="flex  justify-end gap-3">
+          <button onClick={onToggle}>
+            <Check className="text-green-800"/>
+          </button>
+          <button onClick={onEdit}>
+            <Pencil size={20} />
+          </button>
+          <button onClick={onDelete} className="text-red-500">
+            <Trash2 size={20}/>
+          </button>
+        </div>
       </div>
-
-      <div className="flex justify-end gap-3 mt-4">
-        <button onClick={onToggle}><Check /></button>
-        <button onClick={onEdit}><Pencil /></button>
-        <button onClick={onDelete} className="text-red-500"><Trash2 /></button>
-      </div>
+      <p className="text-sm mt-2 text-zinc-400">{item.description}</p>
+      <span className="text-xs bg-gray-950 px-2 py-1 rounded mt-2 w-fit">
+        {item.category}
+      </span>
     </div>
   );
 }
